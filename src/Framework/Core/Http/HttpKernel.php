67c4b3542802e45441982a471d3d8114d5c6ba21
Achraf Soltani <achraf.soltani@gmail.com>
2018-11-20 23:08:08 +0100

    adding Zend Diactoros

---
+++ src/Framework/Core/Http/HttpKernel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: theheretic
+ * Email: soltani.achraf@gmail.com
+ * Date: 11/20/18
+ * Time: 10:02 PM
+ */
+
+namespace Quark\Framework\Core\Http;
+
+
+class HttpKernel
+{
+    protected $request;
+
+    public function __construct()
+    {
+
+    }
+}
